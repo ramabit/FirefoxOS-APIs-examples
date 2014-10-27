bb7516d6f61e7c3179b2b017d6881767a84712cb
ramabit <f.e.ramirez94@gmail.com>
2014-10-27 20:46:51 -0300

    agrego, borro y muestro en consola alarmas seteadas

--- alarm/functions.js
+++ alarm/functions.js
@@ -1,18 +1,49 @@
-agregarAlarma = function(){
-  var myDate  = new Date("15 de agosto, 2014 16:20:00");
+function agregarAlarma(){
+	var horario = prompt("Ingrese el horario de la alarma", new Date());
+	var descripcionAlarma = prompt("Ingrese una descripcion", "");
+	
+	if (horario != null) {
+  	miFecha = new Date(horario);
+		
+		var request = navigator.mozAlarms.add(miFecha, "honorTimezone", descripcionAlarma);
 
-	var data    = {
-  		foo: "bar"
+		request.onsuccess = function () {
+			alert("Alarma agregada correctamente: " + descripcionAlarma);
+		};
+
+		request.onerror = function () { 
+  		console.log("Ocurrio un error: " + this.error.name);
+		};
+	
+	/*	navigator.mozSetMessageHandler("alarm", function (mozAlarm) { 
+  		alert("alarma disparada: " + mozAlarm.data.toString()); 
+		});
+	
+		navigator.mozHasPendingMessage("alarm");
+		*/
 	}
+}
 
-	// The "honorTimezone" string is what make the alarm honoring it
-	var request = navigator.mozAlarms.add(myDate, "honorTimezone", data);
+
+function borrarAlarma(){
+	var id = prompt("Ingrese el el id de la alarma a borrar", "");
+  navigator.mozAlarms.remove(id);
+	alert("Se removio correctamente la alarma " + id)
+}
+
+function obtenerAlarmas(){
+	var request = navigator.mozAlarms.getAll();
 
 	request.onsuccess = function () {
-			alert("Se agrego la alarma correctamente");
+  	this.result.forEach(function (alarm) {
+    console.log('Id: ' + alarm.id);
+    console.log('fecha: ' + alarm.date);
+    console.log('Timezone: ' + alarm.respectTimezone);
+    console.log('data: ' + JSON.stringify(alarm.data));
+  	});
 	};
 
 	request.onerror = function () { 
-  		console.log("Ocurrio un error: " + this.error.name);
+  	console.log("Ocurrio un error: " + this.error.name);
 	};
-}			
+}
