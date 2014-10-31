c5078c16d50e156821b35d921060e096e488b04e
ramabit <f.e.ramirez94@gmail.com>
2014-10-31 00:43:51 -0300

    agrego funciones mobile network info api

--- mobile-network-info/functions.js
+++ mobile-network-info/functions.js
@@ -1,4 +1,22 @@
+function obtenerNombreRedLargo(){
+  return navigator.mozMobileConnection.data.network.longName;
+}
+
+function obtenerNombreRedCorto(){
+  return navigator.mozMobileConnection.data.network.shortName;
+}
+
+function obtenerMCC(){
+  return navigator.mozMobileConnection.data.network.mcc;
+}
+
+function obtenerMNC(){
+  return navigator.mozMobileConnection.data.network.mnc;
+}
 
 
+function obtenerEstadoRed(){
+  return navigator.mozMobileConnection.data.network.state;
+}
 
 
