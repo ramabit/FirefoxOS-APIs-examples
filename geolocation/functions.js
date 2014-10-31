6fa7d003742d893e377d5e56f74190027b29b708
ramabit <f.e.ramirez94@gmail.com>
2014-10-31 00:25:13 -0300

    completo ejemplo de geolocation

--- geolocation/functions.js
+++ geolocation/functions.js
@@ -3,19 +3,28 @@ function encontrame() {
   var output = document.getElementById("out");
   
   if (!navigator.geolocation){
-    output.innerHTML = "<p>La geolocalizacion no esta soportada por su navegador</p>";
+    locacion.innerHTML = "<p>La geolocalizacion no esta soportada por su navegador</p>";
     return;
   }
 
    function geo_success(position) {
-    document.getElementById("locacion").value = "";
-    locacion.value = "Mi posicion actual: \n Latitud: "+ position.coords.latitude + "\n Longitud: " + position.coords.longitude;
+     
+    var latitud  = position.coords.latitude;
+    var longitud = position.coords.longitude;
+    
+    locacion.innerHTML = "Latitud: "+ latitud + "<br> Longitud: " + longitud;
+    
+    var img = new Image();
+    img.src = "http://maps.googleapis.com/maps/api/staticmap?center=" + latitud + "," + longitud + "&zoom=13&size=300x300&sensor=false";
+
+    locacion.appendChild(img);
    }
 
    function geo_error() {
     alert("Su posicion no esta disponible");
    }
-
+   
+   locacion.innerHTML = "Buscando .."
    navigator.geolocation.getCurrentPosition(geo_success, geo_error);
 }
 
